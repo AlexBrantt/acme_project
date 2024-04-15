a37ea7efb30238d9f12152f350286cd8c281c7c6
Алекс Брант <cantstop923@ya.ru>
2024-04-15 21:23:57 +0200

    com

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -8,6 +8,7 @@ from .validators import real_age
 
 # Импортируем класс ошибки валидации.
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
 
 # Множество с именами участников Ливерпульской четвёрки.
 BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
@@ -31,8 +32,7 @@ class BirthdayForm(forms.ModelForm):
     class Meta:
         # Указываем модель, на основе которой должна строиться форма.
         model = Birthday
-        # Указываем, что надо отобразить все поля.
-        fields = '__all__'
+        exclude = ('author',)
 
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'})
@@ -51,6 +51,13 @@ class BirthdayForm(forms.ModelForm):
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
         if f'{first_name} {last_name}' in BEATLES:
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
             )
